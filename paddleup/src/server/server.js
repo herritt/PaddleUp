d9cbf183df3b140c4cd7c62bdb664e00b047a94c
Randy Herritt <randy.herritt@gmail.com>
2023-06-17 12:06:07 -0300

    Added MongoDB and Mongoose. Can now connect to MongoDB database hosted on Atlas. (Closes GH-7)

--- paddleup/src/server/server.js
+++ paddleup/src/server/server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3001;
 
@@ -13,3 +16,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+//mongodb setup
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+//connect to mongoDB using mongoose
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("Database connection error", err));
